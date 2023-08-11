440b8d0f4a590b829421072fefb636591dcac593
Light-Lens <QCoreNest@gmail.com>
2023-08-11 18:28:09 +0530

    clean database indexing code

--- main.py
+++ main.py
@@ -25,24 +25,6 @@ def database():
 
     return lemmatize_titles
 
-
-
-    # titles = [
-    #     i["Title"]
-    #     for i in data
-    # ]
-
-    # tokenize_titles = [tokenize(sent.lower()) for sent in titles]
-    # clean_titles = [stop_words(tok) for tok in tokenize_titles]
-
-    # lemmatize_titles = [
-    #     (idx, " ".join([lemmatize(word) for word in toks]))
-    #     for idx, toks in enumerate(clean_titles)
-    # ]
-
-    # unique_strings = set()
-    # return list(filter(lambda x: x[1] not in unique_strings and not unique_strings.add(x[1]), lemmatize_titles))
-
 def clean_sentence(text):
     toks = tokenize(text.lower())
     clean_toks = stop_words(toks)
