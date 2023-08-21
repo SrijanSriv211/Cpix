1a181568b5ed5bc9df17004f1d3f1a341578bd8b
Light-Lens <QCoreNest@gmail.com>
2023-08-21 22:48:43 +0530

    update build.bat

--- build.bat
+++ build.bat
@@ -1,22 +1,22 @@
 @echo off
 
 if "%1" == "clean" (
-    if exist bin (
+    if exist bin\ (
         rmdir bin /S /Q
     )
 )
 
-if not exist bin/ (
-    mkdir bin
+if not exist bin\ (
+    mkdir bin\data bin\web
 )
 
-pyinstaller.exe --icon="web/static/img/icon.ico" --add-data "web/templates;templates" --add-data "web/static;static" --add-data "data;data" --distpath bin/app.exe --onefile app.py
+pyinstaller.exe --icon="web\static\img\icon.png" --add-data "web\templates;templates" --add-data "web\static;static" --add-data "web\static\img;img" --add-data "data;data" --distpath bin\app.exe --onefile app.py
 
 copy data\* bin\data\*
 copy web\* bin\web\*
 
-rem rmdir /s /q dist
-rem rmdir /s /q build
-rem rmdir /s /q __pycache__
+rmdir dist /S /Q
+rmdir build /S /Q
+rmdir __pycache__ /S /Q
 
-rem del app.spec
+del app.spec
