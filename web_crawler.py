f367aa2eb1048cae12f7486266a87e5a1b40b23d
Light-Lens <QCoreNest@gmail.com>
2023-12-25 18:37:23 +0530

    Update web crawler

--- web_crawler.py
+++ web_crawler.py
@@ -1,71 +1,63 @@
-from search_engines.engines import search_engines_dict
-from search_engines.multiple_search_engines import MultipleSearchEngines, AllSearchEngines
-from search_engines import config
-from utils import arrange_words
-import numpy, json, os
+from colorama import Fore, Style, init
+from bs4 import BeautifulSoup
+import unicodedata, requests, json
 
-class crawler:
-    def __init__(self, search_engines=["google"], filename=config.OUTPUT_DIR+"output", number_of_pages=1):
-        self.search_engines = search_engines
-        self.filename = filename
-        self.number_of_pages = number_of_pages
-        self.ignore_duplicates = True
-        self.proxy = config.PROXY
+# Initialize colorama
+init(autoreset = True)
 
-    def crawl(self, query):
-        timeout = config.TIMEOUT + (10 * bool(self.proxy))
-        engines = [e for e in self.search_engines if e in search_engines_dict or e == 'all']
-        if 'all' in engines:
-            engine = AllSearchEngines(self.proxy, timeout)
+def crawl(url):
+    try:
+        # Make an HTTP request to the specified URL
+        response = requests.get(url)
 
-        elif len(engines) > 1:
-            engine = MultipleSearchEngines(engines, self.proxy, timeout)
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the HTML content of the page
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            # Extract the title of the page
+            title = unicodedata.normalize('NFKD', soup.title.text.strip()) if soup.title else "No Title Found"
+
+            # Extract all the links (URLs) on the page
+            links = [a["href"] for a in soup.find_all("a", href=True) if a["href"].startswith("http")]
+
+            return title, links
 
         else:
-            engine = search_engines_dict[engines[0]](self.proxy, timeout)
+            print(f"Failed to retrieve {url}. Status code: {response.status_code}")
 
-        engine.ignore_duplicate_urls = self.ignore_duplicates
-        engine.search(query, self.number_of_pages)
-        engine.output("json,print", "temp")
-        self.__format_result__()
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-    def __format_result__(self):
-        json_file = open("temp.json", "r", encoding="utf-8")
-        content = json.load(json_file)
-        for engine in content["results"]:
-            for result in content["results"][engine]:
-                site_title = result["title"].split("http")[0]
-                site_link = result["link"]
-                site_link = site_link[:-1] if site_link.endswith("/") else site_link
+data = []
 
-                # Write all the links and titles to a file in json manner.
-                Pages = open(f"{self.filename}.json", "a", encoding="utf-8")
-                Pages.write('{\n')
-                Pages.write(f'\t"Title": "{site_title}",\n')
-                Pages.write(f'\t"URL": "{site_link}"\n')
-                Pages.write('},\n')
-                Pages.close()
+url_to_crawl = "https://en.wikipedia.org/"
+title, links = crawl(url_to_crawl)
+following_links = links
 
-        # Close the Json file and Delete the temp.json file.
-        json_file.close()
-        os.remove("temp.json")
+data.append(
+	{
+		"Title": title,
+		"URL": url_to_crawl
+	},
+)
 
-T1 = [
-    ["class"], ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
-    ["maths", "english", "hindi", "social science", "science", "physics", "chemistry", "biology", "economics", "civics", "history", "geography"],
-    ["chapter"], ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "all"],
-    ["learncbse", "byjus", "vedantu", "tiwari academy", "topper", "brainly", "youtube", "physics wallah", "magnet brains", "unacademy", "pw", "google scholar", "meritnation", "extramarks", "class saathi", "study rankers", "innovative gyan", "jagran josh", "myCBSEguide", "smart study point", "study iq education", "wifistudy", "khan gs research centre", "right to shiksha", 12],
-    ["question answer", "notes", "explanation", "summary", "chapter name", "introduction", "one video", "one shot", "lecture", "in 1 hour", "in 30 minutes", 6]
-]
+print(f"{Fore.YELLOW}{Style.BRIGHT}Crawling the web..")
+for link in following_links:
+    try:
+        title, links = crawl(link)
+        data.append(
+            {
+                "Title": title,
+                "URL": link
+            },
+        )
 
-S1 = ""
-list_of_queries = []
-crawler_engine = crawler(filename="data\\scraped_sites")
-for i in range(10):
-    S1 = " ".join(arrange_words(T1).split())
+        following_links.extend(links)
 
-    if S1 in list_of_queries: continue
-    list_of_queries.append(S1)
+    except KeyboardInterrupt:
+        break
 
-    print(f"\n{S1}")
-    crawler_engine.crawl(S1)
+print(f"{Fore.YELLOW}{Style.BRIGHT}Saving the crawled data..")
+with open("index.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
