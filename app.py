12d4c6afe275ed958fcf8946898a5f22a410096e
SrijanSriv211 <QCoreNest@gmail.com>
2025-03-20 17:54:28 +0530

    do everything in a single html file

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
 from flask import Flask, render_template, request
 from colorama import Fore, Style, init
 from src.color.color import Color
+from src.llm import GROQ
 from pprint import pprint
 import time, os
 
-# Initialize colorama & Color
+# Initialize
 init(autoreset = True)
 C = Color("data\\index.json", "data\\index_hash_map.json")
+llm = GROQ("cache\\GroqAPI.txt")
 
 app = Flask(__name__, template_folder="src\\web\\templates", static_folder="src\\web\\static")
 user_history_path = "src\\web\\history\\user_history.txt"
+
+history = []
 if os.path.isfile(user_history_path) == False:
     with open(user_history_path, "w", encoding="utf-8") as f:
         f.write("")
 
+else:
+    with open(user_history_path, "r", encoding="utf-8") as f:
+        history = [i.strip() for i in f.readlines()]
+
 @app.get("/")
 def index_get():
-    return render_template("index.html")
+    return render_template("index.html", histories=history, overview="Hello! How can I help you today?")
 
 @app.route("/", methods=["POST"])
 def search():
@@ -25,11 +33,14 @@ def search():
 
     # Save the search query in the user history
     print(f"{Fore.YELLOW}{Style.BRIGHT}Saving History")
-    with open(user_history_path, "a", encoding="utf-8") as f:
-        f.write(text + "\n")
+    if text not in history:
+        history.insert(0, text)
+        with open(user_history_path, "w", encoding="utf-8") as f:
+            f.write("\n".join(history) + "\n")
 
     # Use my Color search algo to search.
     start_time = time.time()
+    overview = llm.generate(text)
     results = C.search(text)
     end_time = time.time()
 
@@ -38,29 +49,19 @@ def search():
     print(f"{Fore.YELLOW}{Style.BRIGHT}SEARCH QUERY:", text)
     print(f"{Fore.YELLOW}{Style.BRIGHT}RESULTS:")
     pprint(results)
+    print(f"{Fore.YELLOW}{Style.BRIGHT}AI OVERVIEW:")
+    print(overview)
     print(f"{Fore.WHITE}{Style.BRIGHT}{time_taken}")
 
     # Render the results.
-    return render_template("index.html", results=results, time_taken=time_taken)
-
-@app.route("/history")
-def load_history():
-    # Load user history
-    with open(user_history_path, "r", encoding="utf-8") as f:
-        history = [i.strip() for i in f.readlines()]
-
-    print(history)
-
-    return render_template("history.html", results=history)
+    return render_template("index.html", results=results, time_taken=time_taken, histories=history, overview=overview)
 
 @app.route("/history", methods=["POST"])
 def delete_history():
-    # Delete user history
     with open(user_history_path, "w", encoding="utf-8") as f:
         f.write("")
 
     print("History deleted")
-
     return render_template("history.html")
 
 if __name__ == "__main__":
