f660a9bf017b8dee190969ee7f2d9ac4f9af9e90
SrijanSriv211 <QCoreNest@gmail.com>
2025-03-21 20:42:12 +0530

    Create utils.py

---
+++ src/shared/utils.py
@@ -0,0 +1,29 @@
+from collections.abc import Iterable
+
+# https://stackoverflow.com/a/2158532/18121288
+def flatten(xs):
+    for x in xs:
+        if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
+            yield from flatten(x)
+
+        else:
+            yield x
+
+def calc_total_time(seconds):
+    # Separate the integer part (for hours, minutes, and seconds) from the fractional part (for milliseconds)
+    sec_int, millis = divmod(seconds, 1)
+    millis = int(millis * 1000)  # Convert the fractional part to milliseconds
+
+    min, sec = divmod(int(sec_int), 60)
+    hour, min = divmod(min, 60)
+    hours, minutes, seconds = int(hour), int(min), int(sec)
+
+    t = [
+        f"{hours} hour" + ("s" if hours > 1 else "") if hours > 0 else None,
+        f"{minutes} minute" + ("s" if minutes > 1 else "") if minutes > 0 else None,
+        f"{seconds} second" + ("s" if seconds > 1 else "") if seconds > 0 else None,
+        f"{millis} ms" if millis > 0 else None
+    ]
+    t = list(filter(None, t))
+
+    return ", ".join(t) if t else "0 seconds"
