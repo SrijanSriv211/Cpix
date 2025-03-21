5985802ccf0a088d3e729c0d8387abdfe0bf00da
SrijanSriv211 <QCoreNest@gmail.com>
2025-03-21 20:43:27 +0530

    index using all stages of encoding

--- indexer.py
+++ indexer.py
@@ -1,27 +1,35 @@
-from src.utils import clean_sentence
+from src.shared.nltk_utils import clean_sentence
+from src.shared.utils import flatten
+from src.llm.encoder import Encoder
+from collections import defaultdict
 import json
+
+enc = Encoder()
+enc.load("models\\enc.bin")
 
 def load_crawled_websites(path):
     with open(path, "r", encoding="utf-8") as f:
         return json.load(f)
 
 def inverse_indexing(data):
-    hash_map = {}
+    local_hash_map = []
+    hash_map = defaultdict(list)
 
     for i, x in enumerate(data):
-        title = x["Title"]
-        cleaned_sentence = clean_sentence(title)
-        keywords = cleaned_sentence.split()
+        cleaned_sentence = clean_sentence(x["Title"].lower())
+        ids = list(set(flatten(enc.encode(cleaned_sentence))))
+        idx = [i] * len(ids)
 
-        for keyword in keywords:
-            if keyword in hash_map:
-                hash_map[keyword].append(i)
-                hash_map[keyword] = list(set(hash_map[keyword]))
+        local_hash_map.append(dict(zip(ids, idx)))
 
-            else:
-                hash_map[keyword] = [i]
+    # loop through each dictionary and append values to the respective keys
+    for d in local_hash_map:
+        for key, value in d.items():
+            hash_map[key].append(value)
+            hash_map[key] = list(set(hash_map[key]))
 
-    return hash_map
+    # convert defaultdict to a normal dictionary
+    return dict(hash_map)
 
 def save_hash_map(hash_map, path):
     with open(path, "w", encoding="utf-8") as f:
